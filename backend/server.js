1d02d89f13a41e08c0af061514fa931f439c6524
Adnan Vellicheri <incubation.ngo@gmail.com>
2026-06-22 01:50:35 +0530

    Set trust proxy setting in server configuration

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ require('dotenv').config({ path: path.join(__dirname, '../.env') });
 const apiRouter = require('./routes/api');
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 5000;
 
 // 1. Basic Security Headers (Helmet)
